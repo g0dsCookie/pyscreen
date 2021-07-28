f726ca8d8370cc2907197fd790b8cc7fffb99ab8
g0dsCookie <g0dscookie@cookieprojects.de>
2021-07-28 17:44:42 +0200

    added kubernetes pods menu

---
+++ pyscreen/plugins/kubernetes.py
@@ -0,0 +1,63 @@
+import socket
+from typing import Dict, Any
+
+import kubernetes.client
+import kubernetes.config
+
+from pyscreen.display import Display
+from pyscreen.plugin import Plugin, Author
+from pyscreen.menu import Menu
+
+
+class Kubernetes(Plugin):
+    def __init__(self):
+        displays = {}
+        menus = {"pods": PodsMenu}
+        gpios = {}
+
+        super().__init__("Kubernetes", "1.0.0",
+                         Author("g0dsCookie", "g0dscookie@cookieprojects.de"),
+                         displays=displays, menus=menus, gpios=gpios)
+
+
+class PodsMenu(Menu):
+    def __init__(self, cfg: Dict[str, Any]) -> None:
+        super().__init__(cfg)
+
+        if not cfg.get("api_key"):
+            kubernetes.config.load_kube_config()
+            self._config = kubernetes.client.configuration
+        else:
+            self._config = kubernetes.client.Configuration()
+            self._config.host = cfg.get("host", "https://localhost:6443")
+            self._config.api_key_prefix["authorization"] = "Bearer"
+            self._config.api_key["authorization"] = cfg.get("api_key")
+            self._config.verify_ssl = cfg.get("verify_ssl", True)
+
+        self._nodename = socket.gethostname()
+
+        interval = cfg.get("interval", {})
+        self._add_update_cache("pods", int(interval.get("interval", 30)), self._update_pods)
+
+    @property
+    def pods(self) -> Dict[str, int]: return self._get_cache("pods")
+
+    def _update_pods(self):
+        with kubernetes.client.ApiClient(self._config) as api_client:
+            v1 = kubernetes.client.CoreV1Api(api_client)
+            ret = v1.list_pod_for_all_namespaces(watch=False)
+            started, ready, scheduled = 0, 0, 0
+            for pod in ret.items:
+                if not pod.spec.node_name == self._nodename:
+                    continue
+                scheduled += 1
+                if pod.status.container_statuses[0].started:
+                    started += 1
+                if pod.status.container_statuses[0].ready:
+                    ready += 1
+            return {"started": started, "ready": ready, "scheduled": scheduled}
+
+    def _update(self, display: Display):
+        display.write_line("Scheduled: {:2d}".format(self.pods["scheduled"]))
+        display.write_line("Started:   {:2d}".format(self.pods["started"]))
+        display.write_line("Ready:     {:2d}".format(self.pods["ready"]))
